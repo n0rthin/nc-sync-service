b7cda50ac0c264950f60915b7ca4503fdb6cfe9e
Vlad Severin <vlad.severin@wtt-solutions.com>
2023-03-08 14:35:03 +0200

    retry openai api request if failed

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,11 +1,18 @@
 import openai
 import pinecone
 import os
+from tenacity import (
+  retry,
+  stop_after_attempt,
+  wait_random_exponential,
+) 
+
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
 pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment=os.environ["PINECONE_ENV"])
 index = pinecone.Index(os.environ["PINECONE_INDEX_NAME"])
 
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(7))
 def get_embeddings(texts, model=EMBEDDING_MODEL):
   result = openai.Embedding.create(
     model=model,
